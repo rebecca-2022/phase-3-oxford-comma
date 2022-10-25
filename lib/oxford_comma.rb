08c2a81fc1503f46bfff42878c13300d25aa1f6a
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-25 13:53:00 +0300

    complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    case
+        when array.length == 1 then array.join()
+        when array.length == 2 then array.join(' and ')
+        when array.length >= 3 then 
+            last_element_removed = array.pop()  #remove the last element and add 'and
+            array << ('and ')
+            list_with_commas = array.join(', ') #add commas
+            fomatted_list = list_with_commas + last_element_removed #concatenate the list
+            fomatted_list
+    end
 end
